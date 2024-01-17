feaa14ce9833b68d5f4f669698f2d031df1d935f
caiodansk <124446768+caiodansk@users.noreply.github.com>
2024-01-17 13:14:56 -0300

    Add files via upload

---
+++ estacionamento (1).js
@@ -0,0 +1,40 @@
+var input = require("readline-sync");
+var listavagas = new Array(10);
+function menu() {
+  console.log("1 Registrar entrada\n2 Registrar saida\n3 vagas\n0 Sair");
+}
+
+function registrarEntrada() {
+  var placa = input.question("placa");
+  var entrada = new Date();
+  var carro = { placa: placa, entrada: entrada, saida: null };
+  listavagas.push(carro);
+}
+
+function registrarSaida() {
+  var placa = input.question("placa");
+  var saida= new Date ();
+  var carro = {placa:placa,saida:saida,carro:carro}
+}
+
+function listavagass() {
+  console.log(listavagas);
+}
+menu();
+var opcaoEscolhida = Number(input.question("Digite a opcao "));
+while (opcaoEscolhida !== 0) {
+  switch (opcaoEscolhida) {
+    case 1:
+      registrarEntrada();
+      break;
+    case 2:
+      registrarSaida();
+      break;
+    case 3:
+      listavagass();
+      break;
+  }
+  input.question("contiuar... ");
+  menu();
+  var opcaoEscolhida = Number(input.question("Digite a opcao "));
+}
